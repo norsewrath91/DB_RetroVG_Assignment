7b831324d02a93be75d8eeec17577c4be7845f42
norsewrath91 <nicholas.nocella@gmail.com>
2015-12-04 15:54:05 -0700

    Create popDB.sql

---
+++ popDB.sql
@@ -0,0 +1,38 @@
+DELETE FROM playerinfo;
+DELETE FROM collectorinfo;
+DELETE FROM game;
+DELETE FROM platform;
+
+INSERT INTO platform VALUES ("N64", 1996, "Nintendo");
+INSERT INTO platform VALUES ("NES", 1985, "Nintendo");
+INSERT INTO platform VALUES ("SNES", 1991, "Nintendo");
+INSERT INTO platform VALUES ("Gameboy", 1989, "Nintendo");
+INSERT INTO platform VALUES ("Wii", 2006, "Nintendo");
+INSERT INTO platform VALUES ("Gamecube", 2001, "Nintendo");
+
+
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Mario Kart 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
+INSERT INTO game VALUES ("Super Mario 64","Nintendo", "Platformer","N64","Nintendo EAD","NTSC", 1996);
